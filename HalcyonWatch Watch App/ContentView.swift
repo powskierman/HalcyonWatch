4add3fbb81f88a0c2dbdf94f24238ec188f7ac0b
powskierman <mlapoint99@hotmail.com>
2024-02-20 11:14:50 -1000

    Initial Commit

---
+++ HalcyonWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HalcyonWatch Watch App
+//
+//  Created by Michel Lapointe on 2024-02-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HalcyonWatch Watch App/HalcyonWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalcyonWatchApp.swift
+//  HalcyonWatch Watch App
+//
+//  Created by Michel Lapointe on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct HalcyonWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
